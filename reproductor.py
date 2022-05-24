3829cdf8fc1b5204de5a904eb94cb318493c338b
Dina Tuesta <102607439+DinaTuesta27@users.noreply.github.com>
2022-05-23 19:36:44 -0500

    Update reproductor.py

--- reproductor.py
+++ reproductor.py
@@ -1,18 +1,5 @@
 #Reproductor musica
 #Importar librerias
-<<<<<<< HEAD
-from tkinter import* #tkinder nos sirve para crear la interface.
-from tkinter import filedialog
-from pygame import mixer
-
-class musica:
-    def __init__(self,ventana):
-        ventana.geometry("270x300")
-        ventana.title("Reproductor")
-        ventana.config(bg="gray",bd="25")
-
-        abrir=Button(ventana,text="Abrir",width=10,bg="yellow",relief="groove",bd="4")
-=======
 from tkinter import* #tkinter funciona para la creacion de una interfaz grafica.
 from tkinter import filedialog 
 from pygame import mixer  #Pygame sirve para cargar musica, reproducir sonidos y mezclador de sonidos.
@@ -21,10 +8,9 @@ class musica:
     def __init__(self,ventana): #_init_ sirve para Inicializar la ventana.
         ventana.geometry("270x300") #Tamaño de la ventana emergente.
         ventana.title("Reproductor") #Titulo de la ventana.
-        ventana.config(bg="#0069c0",relief="ridge",bd="25") #Caracteristicas del fondo de la ventana. Marco de la ventana.
+        ventana.config(bg="#0069c0",bd="25") #Caracteristicas del fondo de la ventana. Marco de la ventana.
 
         abrir=Button(ventana,text="Abrir",width=10,bg="#FF0000",relief="groove",bd="4")
->>>>>>> 29f4948e88873e780d333bd298f3e1e90d00ee71
         abrir.place(x=60,y=50)
         reproducir=Button(ventana,text="Reproducir",width=10,bg="#008000",relief="groove",bd="4")
         reproducir.place(x=60,y=90)
