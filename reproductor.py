039cd6905ba129efcdb367c60a799fe63e186f17
Dina Tuesta <102607439+DinaTuesta27@users.noreply.github.com>
2022-05-23 19:27:42 -0500

    Update reproductor.py

--- reproductor.py
+++ reproductor.py
@@ -1,6 +1,6 @@
 #Reproductor musica
 #Importar librerias
-from tkinter import*
+from tkinter import* #tkinder nos sirve para crear la interface.
 from tkinter import filedialog
 from pygame import mixer
 
@@ -8,13 +8,13 @@ class musica:
     def __init__(self,ventana):
         ventana.geometry("270x300")
         ventana.title("Reproductor")
-        ventana.config(bg="gray",relief="ridge",bd="25")
+        ventana.config(bg="gray",bd="25")
 
-        abrir=Button(ventana,text="Abrir",width=10,bg="red",relief="groove",bd="4")
+        abrir=Button(ventana,text="Abrir",width=10,bg="yellow",relief="groove",bd="4")
         abrir.place(x=60,y=50)
         reproducir=Button(ventana,text="Reproducir",width=10,bg="blue",relief="groove",bd="4")
         reproducir.place(x=60,y=90)
-        pause=Button(ventana,text="Pausa",width=10,bg="yellow",relief="groove",bd="4")
+        pause=Button(ventana,text="Pausa",width=10,bg="red",relief="groove",bd="4")
         pause.place(x=60,y=130)
         detener=Button(ventana,text="Detener",width=10,bg="white",relief="groove",bd="4")
         detener.place(x=60,y=170)
