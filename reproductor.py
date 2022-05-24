d07ca1fb10d94c43ce52edc6fb54dbbc798b28e4
Juan Nicolas Cetina <junicegu2012@gmail.com>
2022-05-24 17:06:43 -0500

    Update reproductor.py

--- reproductor.py
+++ reproductor.py
@@ -6,16 +6,16 @@ from pygame import mixer  #Pygame sirve para cargar musica, reproducir sonidos y
 #Definimos las caractisristicas de los botones y en que parte de la vantana se van a ubicar
 class musica:
     def __init__(self,ventana): #_init_ sirve para Inicializar la ventana.
-        ventana.geometry("270x300") #Tamaño de la ventana emergente.
+        ventana.geometry("280x300") #Tamaño de la ventana emergente.
         ventana.title("Reproductor") #Titulo de la ventana.
-        ventana.config(bg="#0069c0",bd="25") #Caracteristicas del fondo de la ventana. Marco de la ventana.
+        ventana.config(bg="#8F9CDA",bd="25") #Caracteristicas del fondo de la ventana. Marco de la ventana.
 
-        ab=Button(ventana,text="Abrir",width=10,bg="#FF0000",relief="groove",bd="4",command=self.abrir)
-        ab.place(x=60,y=50)
-        rep=Button(ventana,text="Reproducir",width=10,bg="#008000",relief="groove",bd="4",command=self.reproducir)
-        rep.place(x=60,y=90)
-        pa=Button(ventana,text="Pausa",width=10,bg="red",relief="groove",bd="4",command=self.pausa)
-        pa.place(x=60,y=130)
+        ab=Button(ventana,text="Abrir",width=10,bg="#0a5cb8",relief="groove",bd="4",command=self.abrir)
+        ab.place(x=70,y=50)
+        rep=Button(ventana,text="Reproducir",width=10,bg="#7f00b2",relief="groove",bd="4",command=self.reproducir)
+        rep.place(x=70,y=90)
+        pa=Button(ventana,text="Pausa",width=10,bg="#a21ad7",relief="groove",bd="4",command=self.pausa)
+        pa.place(x=70,y=130)
         self.abri_musica=False
         self.repro_musica=False
 
