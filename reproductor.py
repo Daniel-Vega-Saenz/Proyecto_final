e43efdae44a3fe4e61c84cadd459076f4eb06711
Daniel Vega Saenz <102559192+Daniel-Vega-Saenz@users.noreply.github.com>
2022-05-23 20:28:04 -0500

    Update reproductor.py

--- reproductor.py
+++ reproductor.py
@@ -10,31 +10,6 @@ class musica:
         ventana.title("Reproductor") #Titulo de la ventana.
         ventana.config(bg="#0069c0",bd="25") #Caracteristicas del fondo de la ventana. Marco de la ventana.
 
-<<<<<<< HEAD
-        abrir=Button(ventana,text="Abrir",width=10,bg="#2196f3",relief="groove",bd="4",command=abrir)
-        abrir.place(x=60,y=50)
-        reproducir=Button(ventana,text="Reproducir",width=10,bg="#7f00b2",relief="groove",bd="4",command=reproducir)
-        reproducir.place(x=60,y=90)
-        pause=Button(ventana,text="Pausa",width=10,bg="#ab3ed8",relief="groove",bd="4",command=pause)
-        pause.place(x=60,y=130)
-        self.abri_musica=False
-        self.repro_musica=False
-
-        def abrir(self):
-            self.abri_musica=filedialog.askopenfilename()
-        def reproducir(self):
-            if self.abri_musica:
-                mixer.music.load(self.abri_musica)
-                mixer.music.play()
-        def pause(self):
-            if self.repro_musica:
-                mixer.musica.pause()
-                self.repro_musica=False
-            else:
-                mixer.music.unpause()
-                self.repro_musica=True
-
-=======
         ab=Button(ventana,text="Abrir",width=10,bg="#FF0000",relief="groove",bd="4",command=self.abrir)
         ab.place(x=60,y=50)
         rep=Button(ventana,text="Reproducir",width=10,bg="#008000",relief="groove",bd="4",command=self.reproducir)
@@ -59,7 +34,6 @@ class musica:
         else:
             mixer.music.unpause()
             self.repro_musica=True
->>>>>>> cecc77bc1fc125fc9f18f824fa90f34b7a548633
 
 ventana=Tk()
 musica(ventana)
