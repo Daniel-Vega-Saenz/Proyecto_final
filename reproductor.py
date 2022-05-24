a8c18757ff093df8de42f61f7e97024cbf75dbdc
Daniel Vega Saenz <102559192+Daniel-Vega-Saenz@users.noreply.github.com>
2022-05-23 19:21:24 -0500

    Update reproductor.py

--- reproductor.py
+++ reproductor.py
@@ -1,23 +1,21 @@
 #Reproductor musica
 #Importar librerias
-from tkinter import* #Hola como estas
-from tkinter import filedialog
-from pygame import mixer
+from tkinter import* #tkinter funciona para la creacion de una interfaz grafica.
+from tkinter import filedialog 
+from pygame import mixer  #Pygame sirve para cargar musica, reproducir sonidos y mezclador de sonidos.
 
 class musica:
-    def __init__(self,ventana):
-        ventana.geometry("270x300")
-        ventana.title("Reproductor")
-        ventana.config(bg="gray",relief="ridge",bd="25")
+    def __init__(self,ventana): #_init_ sirve para Inicializar la ventana.
+        ventana.geometry("270x300") #Tamaño de la ventana emergente.
+        ventana.title("Reproductor") #Titulo de la ventana.
+        ventana.config(bg="#0069c0",relief="ridge",bd="25") #Caracteristicas del fondo de la ventana.
 
-        abrir=Button(ventana,text="Abrir",width=10,bg="red",relief="groove",bd="4")
+        abrir=Button(ventana,text="Abrir",width=10,bg="#FF0000",relief="groove",bd="4")
         abrir.place(x=60,y=50)
-        reproducir=Button(ventana,text="Reproducir",width=10,bg="blue",relief="groove",bd="4")
+        reproducir=Button(ventana,text="Reproducir",width=10,bg="#008000",relief="groove",bd="4")
         reproducir.place(x=60,y=90)
         pause=Button(ventana,text="Pausa",width=10,bg="yellow",relief="groove",bd="4")
         pause.place(x=60,y=130)
-        detener=Button(ventana,text="Detener",width=10,bg="white",relief="groove",bd="4")
-        detener.place(x=60,y=170)
         self.abri_musica=False
         self.repro_musica=False
 
