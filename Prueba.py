023df4ffe50b3e08e36066514eabb977e99f589c
Daniel Vega Saenz <102559192+Daniel-Vega-Saenz@users.noreply.github.com>
2022-05-26 21:33:40 -0500

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,12 +1,10 @@
-
-from cgi import parse_multipart
 from tkinter import* 
 from tkinter import filedialog 
 import pygame
 ventana=Tk() 
 ventana.geometry("500x300") 
 ventana.title("SpotyEan") 
-ventana.config(bg="#8F9CDA",bd="25") 
+ventana.config(bg="#191414",bd="25") 
 pygame.mixer.init()
 
 def añadir():
@@ -63,16 +61,16 @@ def pausa():
         pygame.mixer.music.pause()
         pausar=True
 
-pantalla=Listbox(bg="#FF5733", fg="blue",width=60,selectbackground="white", selectforeground="black")
+pantalla=Listbox(bg="#1db954", fg="blue",width=60,selectbackground="white", selectforeground="black")
 pantalla.pack(pady=20)
-ag=Button(ventana,text="Abrir",width=10,bg="red",relief="groove",bd="4",command=añadir)
+ag=Button(ventana,text="Abrir",width=10,bg="#FFFFFF",relief="groove",bd="4",command=añadir)
 ag.place(x=1,y=190)
-rep=Button(ventana,text="Reproducir",width=10,bg="#7f00b2",relief="groove",bd="4",command=play)
+rep=Button(ventana,text="Reproducir",width=10,bg="#46cacc",relief="groove",bd="4",command=play)
 rep.place(x=100,y=190)
-ant=Button(ventana,text="Anterior",width=10,bg="#a21ad7",relief="groove",bd="4",command=anterior)
+ant=Button(ventana,text="Anterior",width=10,bg="#63bccc",relief="groove",bd="4",command=anterior)
 ant.place(x=199,y=190)
-sg=Button(ventana,text="Siguiente",width=10,bg="blue",relief="groove",bd="4",command=siguiente)
+sg=Button(ventana,text="Siguiente",width=10,bg="#cdb3d6",relief="groove",bd="4",command=siguiente)
 sg.place(x=298,y=190)
-pa=Button(ventana,text="Pausa",width=10,bg="white",relief="groove",bd="4",command=pausa)
+pa=Button(ventana,text="Pausa",width=10,bg="#e7b7c7",relief="groove",bd="4",command=pausa)
 pa.place(x=397,y=190)
 ventana.mainloop()
